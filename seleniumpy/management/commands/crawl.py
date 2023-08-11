755a8b43c70e30750e085f1dcdc1a3fe1271ac47
kkamara <kamaracomputers@gmail.com>
2023-08-11 20:18:52 +0100

    Add validators

--- seleniumpy/management/commands/crawl.py
+++ seleniumpy/management/commands/crawl.py
@@ -1,10 +1,14 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 
 import os
 import json
 import time
 import logging
+import phonenumbers
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -25,6 +29,15 @@ class Command(BaseCommand):
         config_file = open(
             os.path.join(settings.BASE_DIR, 'config.json'),'r')
         config = json.loads(config_file.read())
+
+        if True is not self.validate_url(config['email_page_link']):
+            raise ValidationError('The email_page_link config value must be a valid url.')
+        
+        if True is not self.validate_email(config['email']):
+            raise ValidationError('The email config value must be a email.')
+        
+        if True is not self.validate_phonenumber(config['phone_number']):
+            raise ValidationError('The phone_number config value must be a phone number.')
         
         try:
             chrome_options = webdriver.ChromeOptions()
@@ -32,7 +45,7 @@ class Command(BaseCommand):
             chrome_options.add_argument('--disable-gpu')
             chrome_options.add_argument('--no-sandbox')
             chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-            userAgent = 'Mozilla/5.0 (BB10; Touch) AppleWebKit/537.1+ (KHTML, like Gecko) Version/10.0.0.1337 Mobile Safari/537.1+'
+            userAgent = 'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
             chrome_options.add_argument('--user-agent='+userAgent)
             # Use SELENIUM_HEADLESS in .env to remove GUI.
             if settings.SELENIUM_HEADLESS == True or settings.APP_ENV == 'testing':
@@ -41,7 +54,7 @@ class Command(BaseCommand):
                 chrome_options.add_argument('--disable-dev-shm-usage')
             browser = webdriver.Chrome(options=chrome_options)
             
-            browser.get('https://mail.google.com/mail/u/0/#search/label%3Aunread+newer%3A08-11-2023/FMfcgzGtwWMNnXpvcBBmMCKfbqLDVXWS')
+            browser.get(config['email_page_link'])
 
             el = browser.find_element(
                 By.XPATH, "//input[@id='identifierId']")
@@ -49,7 +62,7 @@ class Command(BaseCommand):
             el.send_keys(config['email']+Keys.ENTER)
 
             del el
-            time.sleep(1)
+            time.sleep(3)
 
             browser.find_element(
                 By.XPATH, "//input[@id='password']").send_keys(
@@ -76,3 +89,24 @@ class Command(BaseCommand):
         if el:
             el_text = browser.execute_script('return arguments[0].innerText', el)
             logging.info(el_text)
+    
+    def validate_url(self, subject):
+        validat_url = URLValidator()
+        try:
+            validat_url(subject)
+            return True
+        except ValidationError as exception:
+            logging.info(str(exception))
+            return False
+    
+    def validate_email(self, subject):
+        try:
+            validate_email(subject)
+            return True
+        except ValidationError as exception:
+            logging.info(str(exception))
+            return False
+    
+    def validate_phonenumber(self, subject):
+        return True if phonenumbers.parse(
+            subject, None) is not None else False
